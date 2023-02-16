ebf85c4fcaefa25b767c1cd6863813771cdf2742
hackerdev24 <122537368+hackerdev24@users.noreply.github.com>
2023-02-16 22:38:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-let question = document.querySelectorAll(".question");
+// this one toggles only one open at a time
+// Ref  - http://stackoverflow.com/questions/37745154/only-open-one-accordion-tab-at-one-time
+<script>
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-window.addEventListener("click", event => {
-  question.forEach(question => {
-    const active = document.querySelector(".question.active");
-    if(active && active !== question ) {
-      active.classList.toggle("active");
-      active.nextElementSibling.style.maxHeight = 0;
+
+var acc = document.getElementsByClassName("question");
+var panel = document.getElementsByClassName('answer');
+
+for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+        var setClasses = !this.classList.contains('active');
+        setClass(acc, 'active', 'remove');
+        setClass(panel, 'show', 'remove');
+
+        if (setClasses) {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
     }
-    question.classList.toggle("active");
-    const answer = question.nextElementSibling;
-    if(question.classList.contains("active")){
-      answer.style.maxHeight = answer.scrollHeight + "px";
-    } else {
-      answer.style.maxHeight = 0;
+}
+
+function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
     }
-  })
+}
+
 });
+</script>
